02fdb4f6b2c9e1a93b01015bb0269ff6c8fd59a8
Akash Deshpande <deshpandeakash47@gmail.com>
2021-10-30 22:49:38 +0530

    Completed till part 4

--- src/App.js
+++ src/App.js
@@ -1,125 +1,146 @@
-import './App.css';
 import { useEffect, useState } from "react";
+import "./App.css";
 
-
-function App() {
-
-  // need state to keep track of todos
-  // because localstorage is synchronus - that could slow down the application
-  // instead of using just an empty array as the initial state - we can use a function in its place,
-  // which will be executed on the initial render
-  // reference: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
-  const[todos, setTodos] = useState(() => {
-    // get the todos from local storage
+export default function App() {
+  const [todos, setTodos] = useState(() => {
     const savedTodos = localStorage.getItem("todos");
-    // if there are todos stored
     if (savedTodos) {
-      // return the parsed JSON object back to a javascript object
       return JSON.parse(savedTodos);
-      // otherwise
     } else {
-      // return an empty array
       return [];
     }
   });
-
-  // need state to keep track ov value in the input
   const [todo, setTodo] = useState("");
+  // boolean state to know if we are editing (this will let us display
+  // different inputs based on a condition (conditional rendering)
+  const [isEditing, setIsEditing] = useState(false);
+  // object state to set so we know which todo item we are editing
+  const [currentTodo, setCurrentTodo] = useState({});
 
-  // useEffect to run once component mounts
   useEffect(() => {
-    // localstorage supports storting strings as keys and values
-    // - therefore we cannot store arrays and objects without converting
-    // the object to a string first. JSON.stringify will convert the object into a JSON string
     localStorage.setItem("todos", JSON.stringify(todos));
-    // add the todos as as a dependency because we want to update
-    // localstorage anytime the todo state changes
   }, [todos]);
 
-  // function to get value of the input and set the new state
   function handleInputChange(e) {
-    // set the new value to what is currently in the input box
     setTodo(e.target.value);
   }
 
-  // function to create a ew object on form submit
+  // function to get the value of the edit input and set the new state
+  function handleEditInputChange(e) {
+    // set the new state value to what's currently in the edit input box
+    setCurrentTodo({ ...currentTodo, text: e.target.value });
+    console.log(currentTodo);
+  }
+
   function handleFormSubmit(e) {
-    // prevent the browser default behavior or refreshing the page on submit
     e.preventDefault();
 
-    // don't submit if the input string is empty
-    if(todo !== "") {
-      // set the new todo state (array)
+    if (todo !== "") {
       setTodos([
-        //copy the current values in state
         ...todos,
         {
-          // setting a basic id to identify the object
           id: todos.length + 1,
-          // set a text property to the value of the todo state and
-          // trim the whitespace from the input
           text: todo.trim()
         }
       ]);
     }
 
-    // clear out the input box
     setTodo("");
   }
 
-  // function to remove a todo item from the todo array
+  function handleEditFormSubmit(e) {
+    e.preventDefault();
+
+    // call the handleUpdateTodo function - passing the currentTodo.id and the currentTodo object as arguments
+    handleUpdateTodo(currentTodo.id, currentTodo);
+  }
+
   function handleDeleteClick(id) {
-    // here we are filtering, the idea is to remove an item from todo array on a button click
     const removeItem = todos.filter((todo) => {
-      // return the rest of the todos that don't match the item we are trying to remove
       return todo.id !== id;
     });
-    // removeItem returns a new array - so now we are setting todos to the new array
     setTodos(removeItem);
   }
 
+  // function to edit a todo item
+  function handleUpdateTodo(id, updatedTodo) {
+    // here we are mapping over the todos array - the idea is check if the todo.id matches the id we pass into the function
+    // if the id's match, use the second parameter to pass in the updated todo object
+    // otherwise just use old todo
+    const updatedItem = todos.map((todo) => {
+      return todo.id === id ? updatedTodo : todo;
+    });
+    // set editing to false because this function will be used inside a onSubmit function - which means the data was submited and we are no longer editing
+    setIsEditing(false);
+    // update the todos state with the updated todo
+    setTodos(updatedItem);
+  }
+
+  // function to handle when the "Edit" button is clicked
+  function handleEditClick(todo) {
+    // set editing to true
+    setIsEditing(true);
+    // set the currentTodo to the todo item that was clicked
+    setCurrentTodo({ ...todo });
+  }
+
   return (
-    <div className = "App">
-      {/* create a form element ad pass the handleFormSubmit function to the form
-      using the onSubmit prop*/}
-      <form onSubmit={handleFormSubmit}>
-        {/* create an input element - make sure to add the value prop
-        with state value passed in and the onChane prop to update the state every time
-        something is typed in the inpput */}
-        <input
-          name = "todo"
-          type = "text"
-          placeholder = "Create a new todo"
-          value = {todo}
-          onChange = {handleInputChange}
-        />
-      </form>
+    <div className="App">
+      {/* We need to conditionally render different inputs based on if we are in editing mode */}
+      {isEditing ? (
+        // if we are editing - display the edit todo input
+        // make sure to add the handleEditFormSubmit function in the "onSubmit" prop
+        <form onSubmit={handleEditFormSubmit}>
+          {/* we've added an h2 element */}
+          <h2>Edit Todo</h2>
+          {/* also added a label for the input */}
+          <label htmlFor="editTodo">Edit todo: </label>
+          {/* notice that the value for the update input is set to the currentTodo state */}
+          {/* also notice the handleEditInputChange is being used */}
+          <input
+            name="editTodo"
+            type="text"
+            placeholder="Edit todo"
+            value={currentTodo.text}
+            onChange={handleEditInputChange}
+          />
+          {/* here we added an "update" button element - use the type="submit" on the button which will still submit the form when clicked using the handleEditFormSubmit function */}
+          <button type="submit">Update</button>
+          {/* here we added a "Cancel" button to set isEditing state back to false which will cancel editing mode */}
+          <button onClick={() => setIsEditing(false)}>Cancel</button>
+        </form>
+      ) : (
+        // if we are not editing - display the add todo input
+        // make sure to add the handleFormSubmit function in the "onSubmit" prop
+        <form onSubmit={handleFormSubmit}>
+          {/* we've added an h2 element */}
+          <h2>Add Todo</h2>
+          {/* also added a label for the input */}
+          <label htmlFor="todo">Add todo: </label>
+          {/* notice that the value is still set to the todo state */}
+          {/* also notice the handleInputChange is still the same */}
+          <input
+            name="todo"
+            type="text"
+            placeholder="Create a new todo"
+            value={todo}
+            onChange={handleInputChange}
+          />
+          {/* here we just added a "Add" button element - use the type="submit" on the button which will still submit the form when clicked using the handleFormSubmit function */}
+          <button type="submit">Add</button>
+        </form>
+      )}
 
-    {/* create a ul list to hold all the todo items */}
-    <ul className = "todo-list">
-
-      {/* map over the todos array which creates a new li element for every todo 
-      make sure to add the "key" prop using the unique todo.id value to teh li element
-      remember this is an array of objects - so we need to access the property "text"
-      to get the value we want to display
-      */}
-      {todos.map((todo) => (
-        // now we are adding a simple button that we can click on 
-        <li key = {todo.id}>
-          {/* Add the function we created above as the onClick handler
-          remember the handleDeleteClick function needs to know which item we want to remove
-          so we need to pass the todo.id to the function needs to the function - also a side note,
-          notice how we are calling the handleDeleteClick, this makes sure we are not
-          running the function on page load, but rather when the button is clicked.
-          */}
-          {todo.text}{" "}
-          <button onClick = {() => handleDeleteClick(todo.id)}>X</button>
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+            {/* we are passing the entire todo object to the handleEditClick function*/}
+            <button onClick={() => handleEditClick(todo)}>Edit</button>
+            <button onClick={() => handleDeleteClick(todo.id)}>Delete</button>
           </li>
-      ))}
-    </ul>
-
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
